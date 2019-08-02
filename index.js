3d4efd96044deafab91f2a340671b3e1f528f3ae
Deep Gandhi <deep.gandhi00@gmail.com>
2019-08-02 15:50:20 +0530

    added routes to index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const user = require('./Routes/user');
+const post = require('./Routes/post');
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use('/user',user);
+app.use('/post',post);
 
 app.listen(9000);
